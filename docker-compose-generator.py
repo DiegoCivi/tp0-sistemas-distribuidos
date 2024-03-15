21a9a83dc8ac8bab79d9e382e0fdd6932921a9b7
DiegoCivi <dcivini@fi.uba.ar>
2024-03-15 11:06:18 -0300

    Add docker-compose-generator

---
+++ docker-compose-generator.py
@@ -0,0 +1,55 @@
+import yaml
+import sys
+
+ARGS_QUANTITY = 2
+CLIENT_QUANTITY_INDEX = 1
+
+docker_compose = {
+    'version': '3.9',
+    'name': 'tp0',
+    'services': {
+        'server': {
+            'container_name': 'server',
+            'image': 'server:latest',
+            'entrypoint': 'python3 /main.py',
+            'environment': [
+                'PYTHONUNBUFFERED=1', 'LOGGING_LEVEL=DEBUG'],
+            'networks': ['testing_net']
+        },
+    },
+    'networks': {
+        'testing_net': {
+            'ipam': {
+                'driver': 'default',
+                'config': [{'subnet': '172.25.125.0/24'}]
+            }
+        }
+    }
+}
+
+def main():
+    if len(sys.argv) > ARGS_QUANTITY:
+        print(f"Only one argument needed. Received {len(sys.argv) - 1} arguments.") #TODO: VER COMO SACO ESE 1 
+        return 1
+    
+    try:
+        client_quantity = int(sys.argv[CLIENT_QUANTITY_INDEX])
+    except:
+        print("Argument received could not be parsed.")
+        return 1
+    
+    for id in range(client_quantity):
+        client = f"client{id+1}"
+        docker_compose['services'][client] = {
+            'container_name': client,
+            'image': 'client:latest',
+            'entrypoint': '/client',
+            'environment': [f'CLI_ID={id+1}', 'CLI_LOG_LEVEL=DEBUG'],
+            'networks': ['testing_net'],
+            'depends_on': ['server']
+        }
+
+    with open('docker-compose-dev.yaml', 'w') as file:
+        yaml.dump(docker_compose, file, default_flow_style=False)
+
+main()
