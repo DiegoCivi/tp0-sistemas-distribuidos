5c2877f304bb1d5b732ca2af15aa668736a157f4
DiegoCivi <dcivini@fi.uba.ar>
2024-03-16 20:04:39 -0300

    Add comments

--- scripts/srv-connection/netcat-command.py
+++ scripts/srv-connection/netcat-command.py
@@ -1,4 +1,5 @@
 import os
 
-os.system('echo "hola" | nc server 12345') # TODO: params like port and ip should be read from config file
+# Send message to server with netcat
+os.system('echo "THIS IS A TEST" | nc server 12345') # TODO: params like port and ip should be read from config file. The msg should be received through the dockerfile
 os.system('exit')
--- scripts/srv-connection/server-connection-test.py
+++ scripts/srv-connection/server-connection-test.py
@@ -1,7 +1,13 @@
 import os
 
-# TODO: Add the case where the network is not up
+MSG = "THIS IS A TEST"
 
+# The image from ./Dockerfile is built.
 os.system('docker build -t test-conn ./scripts/srv-connection')
+
+# The container is started from the previous image. It is connected to the same docker network as the server container.
+# Inside the container a simple script that uses netcat to send a message to the server is runned.
 srv_ans = os.popen('docker run --rm --network tp0_testing_net test-conn').read()
-print(f"The server ans was: {srv_ans}")
+
+# Check if the servere is working properly by answering the same message it received.
+print(f"The echo-server works: {srv_ans == MSG}")
